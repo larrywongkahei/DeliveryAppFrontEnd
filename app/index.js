9375325b773d945dff2525ddd02797b4f25b3883
Maryhei <makemak123@gmail.com>
2023-04-01 00:41:42 +0100

    Created a style.js file and created a signin button

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
-import { Text, TextInput, View, ScrollView, SafeAreaView } from "react-native";
+import { Text, TextInput, View, ScrollView, SafeAreaView, Button} from "react-native";
 import { Stack, useRouter } from "expo-router";
+import styles from "../style.style";
 
 const Homepage = () => {
 
@@ -13,21 +14,24 @@ const Homepage = () => {
                 headerTitle: "Driver App",
             }}
             />
-            <TextInput style={{
-                height:40, 
-                color:"#808080", 
-                width:"50%", 
-                textAlign:"center",
-                marginLeft:"25%",
-                justifyContent:"center",
-                marginTop:"30%",
-                borderRadius:"30px",
-                borderColor:"#FFC0CB",
-                borderWidth:2,
-            }}
-                placeholderTextColor="gray"
-                placeholder="Type here"
-                defaultValue=""/>
+
+            <View style={{marginTop:"70%", gap:24}}>
+                <TextInput style={styles.homeInput}
+                    placeholderTextColor="gray"
+                    placeholder="Account Name"
+                    defaultValue=""/>
+
+                <TextInput style={styles.homeInput}
+                    placeholderTextColor="gray"
+                    placeholder="Password"
+                    defaultValue=""/>
+                    
+                <Button 
+                title="Signin"
+                color=""
+                onPress={()=>{}}
+                />
+            </View>
         </SafeAreaView>
     )
 }
---
+++ style.style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native"
+
+
+
+const styles = StyleSheet.create({
+    homeInput:{
+        height:40, 
+        color:"#808080", 
+        width:"50%", 
+        textAlign:"center",
+        marginLeft:"25%",
+        justifyContent:"center",
+        borderRadius:"30px",
+        borderColor:"#FFC0CB",
+        borderWidth:2,
+    }
+
+})
+
+export default styles;
