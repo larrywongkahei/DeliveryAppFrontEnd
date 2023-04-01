c7631eccce7ec1a4aba229c4ad24beeee1a13007
Maryhei <makemak123@gmail.com>
2023-04-01 13:51:14 +0100

    Set up home page

---
+++ app/home.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, View, Text } from "react-native";
+
+const Home = () => {
+    return (
+        <SafeAreaView>
+            <Text>
+                I am home page
+            </Text>
+        </SafeAreaView>
+    )
+}
