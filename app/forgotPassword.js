d6307ae2b4a707643a6f3a4f36feddeea7c1d6fb
Maryhei <makemak123@gmail.com>
2023-04-16 22:59:23 +0100

    Added change password page and functions

--- app/forgotPassword.js
+++ app/forgotPassword.js
@@ -9,12 +9,62 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 const ForgotPassword = () => {
 
     const [showPassword, setShowPassword] = useState(false)
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
 
     const router = useRouter()
 
+    function confirm(){
+        const data = {
+            'email':email, 
+            'password':password
+        }
+        if (checkInput()){
+            if(checkPassword()){
+                API.ChangePassword(data)
+                .then(response => {
+                    if (response.status === 200){
+                        Alert.alert("Successful", 'Password has changed', 
+                        {
+                          text: 'Cancel',
+                          style: 'cancel',
+                        })
+                        router.back()
+                    }
+                })
+            }
+        }
+    }
+
+    function checkInput(){
+        if (password !== "" || confirmPassword !== ""){
+            return true
+        }else{
+            Alert.alert("Failed", 'Passwords could not be empty', 
+            {
+              text: 'Cancel',
+              style: 'cancel',
+            })
+            return false
+        }
+    }
+
+    function checkPassword(){
+        if(password === confirmPassword){
+            return true
+        }else{
+            Alert.alert("Failed", 'Passwords not matched', 
+            {
+              text: 'Cancel',
+              style: 'cancel',
+            })
+        }
+    }
+
 
     function checkUserExist (){
-        API.CheckUser('Aa')
+        API.CheckUser(email)
         .then(response => {
             if(response.status === 400){
                 Alert.alert("Something's wrong", 'Your details doesnt match any existing user', 
@@ -23,16 +73,61 @@ const ForgotPassword = () => {
                   style: 'cancel',
                 })
             }else{
-                console.log('yes')
-                setShowPassword(!showPassword)
+                setShowPassword(true)
             }
         })
     }
     return (
-        <SafeAreaView style={{alignItems:'center'}}>
-            <Text>
+        <SafeAreaView style={{alignItems:'center', marginTop:"25%", gap:30}}>
+            <Text style={{fontSize:25, fontWeight:'bold'}}>
                 Having trouble signing in?
             </Text>
+            <Text style={{fontSize:15}}>
+                Please enter your Email to get started.
+            </Text>
+            <View style={{gap:24, justifyContent:"center", alignItems:"center", width:'80%'}}>
+                {!showPassword ? 
+                <TextInput style={styles.homeInput}
+                    placeholderTextColor="gray"
+                    placeholder="Email address"
+                    value={email}
+                    onChangeText={(input) => {setEmail(input)}}/> : 
+                    <View style={{height:40, width:"75%", borderWidth:2, borderColor:"#FFC0CB", borderRadius:20, justifyContent:'center'}}>
+                        <Text style={{color:"#808080", textAlign:'center'}}>
+                            {email}
+                        </Text>
+                    </View>
+                    }
+                {showPassword ? 
+                <View style={{width:'100%', alignItems:'center', gap:8}}>
+                    <TextInput style={styles.homeInput}
+                    placeholderTextColor="gray"
+                    placeholder="New password"
+                    value={password}
+                    autoCapitalize="none"
+                    onChangeText={(input) => {setPassword(input)}}/>
+
+                    <TextInput style={styles.homeInput}
+                    placeholderTextColor="gray"
+                    placeholder="Confirm Password"
+                    value={confirmPassword}
+                    autoCapitalize="none"
+                    onChangeText={(input) => {setConfirmPassword(input)}}/>
+                </View>  : null}
+                
+                <TouchableOpacity style={{backgroundColor:"#FFC0CB", paddingHorizontal:30, paddingVertical:8, borderRadius:'30%'}} onPress={()=>{
+                    if(!showPassword){
+                        checkUserExist()
+                    }else{
+                        confirm()
+                    }
+                }}>
+                    <Text>
+                        Confirm
+                    </Text>
+                </TouchableOpacity>
+            </View>
+
         </SafeAreaView>
     )
 }
--- helpers/APIs.js
+++ helpers/APIs.js
@@ -1,70 +1,81 @@
 const API = {
-    createUser (newUser){
-        return fetch("https://vast-wave-67131.herokuapp.com/createuser", {
-            method: "post",
-            body: JSON.stringify(newUser),
-            headers: {
-              'Content-type': 'application/json'
-            }
-        })
-        .then(response => {
-            if (response.status === 200){
-                return true
-            }else{
-                return false
-            }})
-    },
-    Login(user) {
-        return fetch('https://vast-wave-67131.herokuapp.com/login', {
-          method: "post",
-          body: JSON.stringify(user),
-          headers: {
-            'Content-type': 'application/json'
-          }
-        })
-        .then(response => {
-            if (response.status === 200){
-                return true
-            }else{
-                return false
-            }})
-    },
-    UpdateUser(data){
-        return fetch('https://vast-wave-67131.herokuapp.com/update', {
-          method: "put",
-          body: JSON.stringify(data),
-          headers: {
-            'Content-type': 'application/json'
-          }
-        })
-    },
-    GetUser(name){
-        return fetch('https://vast-wave-67131.herokuapp.com/data?name='+ name, {
-          method:'get'
-        })
-    },
-    AddToLog(data){
-      return fetch('https://vast-wave-67131.herokuapp.com/addDelivery', {
-          method: "put",
-          body: JSON.stringify(data),
-          headers: {
-            'Content-type': 'application/json'
-          }
+  createUser(newUser) {
+    return fetch("https://vast-wave-67131.herokuapp.com/createuser", {
+      method: "post",
+      body: JSON.stringify(newUser),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+      .then(response => {
+        if (response.status === 200) {
+          return true
+        } else {
+          return false
+        }
       })
-    },
-    DeleteDelivery(data){
-      return fetch('https://vast-wave-67131.herokuapp.com/deleteDelivery', {
-          method: "put",
-          body: JSON.stringify(data),
-          headers: {
-            'Content-type': 'application/json'
-          }
+  },
+  Login(user) {
+    return fetch('https://vast-wave-67131.herokuapp.com/login', {
+      method: "post",
+      body: JSON.stringify(user),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+      .then(response => {
+        if (response.status === 200) {
+          return true
+        } else {
+          return false
+        }
       })
-    },
-    CheckUser(email){
-      return fetch('https://vast-wave-67131.herokuapp.com/data?email='+ email, {
-        method:'get'
-      })
+  },
+  UpdateUser(data) {
+    return fetch('https://vast-wave-67131.herokuapp.com/update', {
+      method: "put",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+  },
+  GetUser(name) {
+    return fetch('https://vast-wave-67131.herokuapp.com/data?name=' + name, {
+      method: 'get'
+    })
+  },
+  AddToLog(data) {
+    return fetch('https://vast-wave-67131.herokuapp.com/addDelivery', {
+      method: "put",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+  },
+  DeleteDelivery(data) {
+    return fetch('https://vast-wave-67131.herokuapp.com/deleteDelivery', {
+      method: "put",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+  },
+  CheckUser(email) {
+    return fetch('https://vast-wave-67131.herokuapp.com/data?email=' + email, {
+      method: 'get'
+    })
+  },
+  ChangePassword(data) {
+    return fetch('https://vast-wave-67131.herokuapp.com/changePassword', {
+      method: "put",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
   },
 }
 
